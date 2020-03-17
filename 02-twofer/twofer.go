fea77b7b69e3571d7b9512619ab502a8ef9cee62
Ian Hanes <ian@sharelocalmedia.com>
2020-03-17 15:44:26 -0400

    Update twofer.go

--- 02-twofer/twofer.go
+++ 02-twofer/twofer.go
@@ -4,11 +4,8 @@ import "fmt"
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	s := "One for %s, one for me."
-	if name != "" {
-		s = fmt.Sprintf(s, name)
-	} else {
-		return fmt.Sprintf(s, "you")
+	if name == "" {
+		name = "you"
 	}
-	return s
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
