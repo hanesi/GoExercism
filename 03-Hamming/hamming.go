639378d262b613600d73c562e1d3de87dea2bc2f
hanesi <ian_hanes@utexas.edu>
2020-03-30 19:54:58 -0400

    Update hamming.go

--- 03-Hamming/hamming.go
+++ 03-Hamming/hamming.go
@@ -2,14 +2,15 @@ package hamming
 
 import "fmt"
 
-// Calculate the hamming distance distance between two DNA string
+// Distance between two DNA strings
 func Distance(a, b string) (int, error) {
 	hammingDistance := 0
-	if len(a) != len(b) {
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
 		return 0, fmt.Errorf("Strings need to be the same length")
 	}
-	for i := range a {
-		if a[i] != b[i] {
+	for i := range ar {
+		if ar[i] != br[i] {
 			hammingDistance++
 		}
 	}
